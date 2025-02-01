d508f6265846e3ed58a4ae27e9e56aedb8ec95a8
John Hilts <johnhilts@live.com>
2025-02-01 10:18:32 +0000

    hokima - continue replacing data-store parameter with DI

--- repl-tests.lisp
+++ repl-tests.lisp
@@ -1,8 +1,18 @@
 ;; save user login index
-(let ((jfh-store:*store-root-folder* "/root/code/lisp/source/chasi"))
-  (let* ((data (jfh-store:serialize-object->list (jfh-user::make-user-index-entry (make-instance 'jfh-user:application-meta-user :user-login "you@there.com" :user-id "20250126-0934")) (list 'jfh-user:user-id 'jfh-user:user-login)))
-         (store-data (make-instance 'jfh-store:user-index-store-data :data data :label "user-login-index")))
-    (jfh-store:save-user-data store-data)))
+(let ((jfh-store:*app-data-path* "/root/code/lisp/source/chasi"))
+  (jfh-store:save-data 
+   (make-instance 'jfh-store:user-index-store :label "user-login-index")
+   (make-instance 'jfh-store:user-index-data :serialized-data 
+                  (jfh-store:serialize-object->list
+                   (jfh-user::make-user-index-entry (make-instance 'jfh-user:application-user :user-login "me2@here.com" :user-id "20250131-2"))
+                   (list 'jfh-user:user-id 'jfh-user:user-login)))))
+
+(jfh-store:save-data 
+ (make-instance 'jfh-store:user-index-store :label "user-login-index")
+ (make-instance 'jfh-store:user-index-data :serialized-data 
+                (jfh-store:serialize-object->list
+                 (jfh-user::make-user-index-entry (make-instance 'jfh-user:application-user :user-login "me2@here.com" :user-id "20250131-2"))
+                 (list 'jfh-user:user-id 'jfh-user:user-login))))
 
 ;; save meta-user
 (let ((jfh-store:*store-root-folder* "/root/code/lisp/source/chasi"))
@@ -17,12 +27,3 @@
   (jfh-user::save-application-user (make-instance 'hokima-web-app::web-app-user :user-id "20250126-1421" :user-name "Mr Web User" :user-login "you@web.com" :user-password "password-not-set")))
 
 ;; (make-instance 'hokima-web-app::web-app-user :user-login "you@web.com" :user-password "password-not-set" :user-id user-id)
-
-;; update user login index
-(let ((jfh-store:*store-root-folder* "/root/code/lisp/source/chasi"))
-  (let* ((data (jfh-store:serialize-object->list (jfh-user::make-user-index-entry (make-instance 'jfh-user:application-meta-user :user-login "you@there.com" :user-id "20250126-1027")) (list 'jfh-user:user-id 'jfh-user:user-login)))
-         (store-data (make-instance 'jfh-store:user-index-store-data :data data :label "user-login-index"))
-         (store-object (make-instance 'jfh-store:user-index-store-object :label (jfh-store::label store-data) :location (format nil "~A/users" jfh-store::*store-root-folder*))))
-    (let ((file-contents (jfh-store:get-data store-object store-data))
-          (file-path (format nil "~A/~A.sexp" (jfh-store::location store-object) (jfh-store::label store-object))))
-      (format nil "File path: ~A~%Updated Data:~A~%" file-path (push (jfh-store::data store-data) file-contents)))))
