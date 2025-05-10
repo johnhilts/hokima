2521d6c56a11ea83b7bde10e976c1479ec254db3
John Hilts <johnhilts@live.com>
2025-05-10 23:01:16 +0000

    hokima - improve object I/O

--- web-app/common/code/user.lisp
+++ web-app/common/code/user.lisp
@@ -20,12 +20,12 @@
 (defmethod user:save-application-user ((web-app-user web-app-user))
   "Input: web-app-user and app-configuration. Output: serialized web-app-user (sub-class specific fields only) . Persist application user info."
   (call-next-method)
-  (jfh-store:save-object web-app-user :readers '(user-name) :key (jfh-user:user-id web-app-user)))
+  (jfh-store:save-object web-app-user)) ;; TODO: don't need save-name?
 
 (defun get-web-user-info (user-id) ;; TODO (maybe?) this has to be converted into a function specializing on user-identifier
   "Derive web-user info from app-user."
   (let* ((application-user (user:get-user-info (make-instance 'jfh-user:application-user-id :user-id user-id)))
          (user-login (user:user-login application-user))
-         (web-user-info (jfh-store:make-instance* 'web-app-user :key (jfh-user:user-id application-user)))
-	 (secure-user-info (jfh-store:make-instance* 'jfh-user:application-secure-user :key (jfh-user:user-id application-user))))
+         (web-user-info (jfh-store:make-instance* 'web-app-user :user-id (jfh-store:user-id application-user)))
+	 (secure-user-info (jfh-store:make-instance* 'jfh-user:application-secure-user :user-id (jfh-store:user-id application-user))))
     (make-web-app-user (user-name web-user-info) user-login (jfh-user:user-password secure-user-info) user-id)))
