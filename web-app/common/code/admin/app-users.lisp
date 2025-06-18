dae1753838c2202823d999aef8a225a48d24f969
John Hilts <johnhilts@live.com>
2025-06-18 06:46:03 +0000

    hokima - improve security

--- web-app/common/code/admin/app-users.lisp
+++ web-app/common/code/admin/app-users.lisp
@@ -3,7 +3,11 @@
 
 (defun add-external-user (user-name user-email user-fingerprint)
   "Add new user for a different app."
-  (let* ((secure-user (make-instance 'user:application-secure-user :user-login user-email :user-password "password-not-set" :user-fingerprint user-fingerprint :user-id ""))
+  (let* ((secure-user (make-instance 'user:application-secure-user
+                                     :user-login user-email
+                                     :user-password "password-not-set"
+                                     :user-fingerprint user-fingerprint
+                                     :user-id ""))
          (web-user (make-external-app-user user-name user-email (jfh-store:user-id secure-user))))
     (user:save-new-application-user secure-user)
     (user:save-application-user web-user)))
